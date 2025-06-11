7d24e0828cd6b8d664e698ed0ae48c7ed715938f
durga.prem.sanisetty <prem.sanisetty@gmail.com>
2025-06-11 15:46:39 +0530

    update in accessing the variables from a key

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "instances"{
   for_each = var.instances
   source = "./module/ec2"
-  app_port = each.key["app_port"]
+  app_port = each.value["app_port"]
   component_name = each.key
   env = var.env
-  instance_type = each.key["instance_type"]
+  instance_type = each.value["instance_type"]
 }
