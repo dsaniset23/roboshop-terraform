9cf72737f6c306d2b0b3806b33df455437150213
dsaniset <prem.sanisetty@gmail.com>
2026-02-02 17:42:59 +0530

    commented

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-module "db_instances"{
-  for_each = var.db_instances
-  source = "./module/ec2"
-  app_port = each.value["app_port"]
-  component_name = each.key
-  env = var.env
-  instance_type = each.value["instance_type"]
-  domain_name = var.domain_name
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-  volume_size = each.value["volume_size"]
-}
+# module "db_instances"{
+#   for_each = var.db_instances
+#   source = "./module/ec2"
+#   app_port = each.value["app_port"]
+#   component_name = each.key
+#   env = var.env
+#   instance_type = each.value["instance_type"]
+#   domain_name = var.domain_name
+#   zone_id = var.zone_id
+#   vault_token = var.vault_token
+#   volume_size = each.value["volume_size"]
+# }
 
 # module "app_instances"{
 #   depends_on = [module.db_instances]
